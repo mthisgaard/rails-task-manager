0e1e60ca8ef6d69658372d4b89fe8b7b4c953c26
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-09 12:03:00 +0900

    create a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create', as: :tasks
+  # Read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
