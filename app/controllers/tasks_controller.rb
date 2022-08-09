ed1077cc416b94acaeee6e2fea403a7a0aab024f
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-09 14:31:19 +0900

    add style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
